5cb152b1fd3ea9a8bc812e159c754467826ebe9a
lucianogiannini <54635097+lucianogiannini@users.noreply.github.com>
2021-04-16 16:33:13 -0400

    updates

--- Plantry/app/src/main/java/com/hfad/plantry/MainActivity.java
+++ Plantry/app/src/main/java/com/hfad/plantry/MainActivity.java
@@ -14,5 +14,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        int one = 0;
+
     }
 }
