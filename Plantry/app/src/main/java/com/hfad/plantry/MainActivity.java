5e85eb359ea4dccd3b7b08657d0027e8f4672cd0
lucianogiannini <54635097+lucianogiannini@users.noreply.github.com>
2021-04-22 21:14:57 -0400

    adding in main activity layout. Adding in button methods and activity classes for each

--- Plantry/app/src/main/java/com/hfad/plantry/MainActivity.java
+++ Plantry/app/src/main/java/com/hfad/plantry/MainActivity.java
@@ -3,10 +3,12 @@ package com.hfad.plantry;
 import androidx.appcompat.app.AppCompatActivity;
 import java.util.Scanner;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     private SQLiteDatabase db;
@@ -17,5 +19,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         SQLiteOpenHelper plantryDatabaseHelper = new PlantryDatabaseHelper(this);
+    }
+    public void goToPantry(View view){
+        Intent intent = new Intent(this, PantryActivity.class);
+        startActivity(intent); //start new activity
+    }
+
+    public void addRecipe(View view){
+        Intent intent = new Intent(this, RecipeActivity.class);
+        startActivity(intent); //start new activity
     }
 }
---
+++ Plantry/app/src/main/java/com/hfad/plantry/PantryActivity.java
@@ -0,0 +1,14 @@
+package com.hfad.plantry;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PantryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantry);
+    }
+}
