386ef0a5db967fee512c78a9ea031ab5ff240694
lucianogiannini <54635097+lucianogiannini@users.noreply.github.com>
2021-04-23 17:00:49 -0400

    adding in button and list view

--- Plantry/app/src/main/java/com/hfad/plantry/RecipeActivity.java
+++ Plantry/app/src/main/java/com/hfad/plantry/RecipeActivity.java
@@ -3,6 +3,7 @@ package com.hfad.plantry;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class RecipeActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class RecipeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
+    }
+
+    public void addRecipeToShoppingList(View view){
+
     }
 }
