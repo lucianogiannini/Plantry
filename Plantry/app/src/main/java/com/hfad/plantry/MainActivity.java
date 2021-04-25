3489960f25034b9c3a6061703057ec6cd966aa0d
lucianogiannini <54635097+lucianogiannini@users.noreply.github.com>
2021-04-25 18:40:22 -0400

    adding in all recipes to front end

--- Plantry/app/src/main/java/com/hfad/plantry/MainActivity.java
+++ Plantry/app/src/main/java/com/hfad/plantry/MainActivity.java
@@ -54,52 +54,171 @@ public class MainActivity extends AppCompatActivity {
             System.out.println(time);
             int imageId = cursor.getInt(5);
             System.out.println(imageId);
-            TextView recipe_of_the_week_textview = (TextView)findViewById(R.id.recipe_of_the_week_textview);
+            TextView recipe_of_the_week_textview = (TextView) findViewById(R.id.recipe_of_the_week_textview);
             recipe_of_the_week_textview.setText(name);
 
-            Button recipe_of_the_week_button = (Button)findViewById(R.id.recipe_of_the_week_button);
+            Button recipe_of_the_week_button = (Button) findViewById(R.id.recipe_of_the_week_button);
 
             Drawable img = recipe_of_the_week_button.getContext().getResources().getDrawable(imageId);
-            recipe_of_the_week_button.setCompoundDrawablesWithIntrinsicBounds( img, null, null, null);
+            recipe_of_the_week_button.setCompoundDrawablesWithIntrinsicBounds(img, null, null, null);
 
             cursor = db.query("RECIPEITEMS",
                     new String[]{"*"},
                     "RECIPE_ID = ?",
-                    new String[] {Integer.toString(randomInt)}, null, null, null);
+                    new String[]{Integer.toString(randomInt)}, null, null, null);
             cursor.moveToFirst();
 
             String recipe_of_the_week_text = "Ingredients:\n";
             recipe_of_the_week_button.setTextSize(10);
 
 
-            for(int i = 0; i <= 6; i++){
-                if(i>0) {
+            for (int i = 0; i <= 6; i++) {
+                if (i > 0) {
                     cursor.moveToNext();
                 }
                 recipe_of_the_week_text += "• ";
                 String tempName = cursor.getString(1);
                 Double tempWeight = cursor.getDouble(2);
                 String tempType = cursor.getString(3);
-                if(!tempType.equals("ounces")){
-                    if(tempType.equals("teaspoon"))
+                if (!tempType.equals("ounces")) {
+                    if (tempType.equals("teaspoon"))
                         recipe_of_the_week_text += convertOuncesToTeaspoon(tempWeight) + " " + tempType + " of " + tempName + "\n";
                     else
                         recipe_of_the_week_text += convertOuncesToTablespoon(tempWeight) + " " + tempType + " of " + tempName + "\n";
                 }//end if
-                else{
+                else {
                     recipe_of_the_week_text += tempWeight + " " + tempType + " of " + tempName + "\n";
                 }//end else
             }//end for loop
-            System.out.println(recipe_of_the_week_text);
-            System.out.println(recipe_of_the_week_button.getText());
+            recipe_of_the_week_text += "Time : " + time + "\t\t" + "Level: " + level;
             recipe_of_the_week_button.setText(recipe_of_the_week_text);
             cursor.close(); //close cursor
             db.close(); //close the database
-        } catch(SQLiteException e) {
+        } catch (SQLiteException e) {
+            Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT); //Display a pop-up message if a SQLiteException is thrown
+            toast.show();
+        }
+
+
+        //adding in all the data from all recipes
+
+
+        for (int i = 0; i < 6; i++) {
+            plantryDatabaseHelper = new PlantryDatabaseHelper(this);
+            try {
+                db = plantryDatabaseHelper.getReadableDatabase();
+                cursor = db.query("RECIPE",
+                        new String[]{"*"},
+                        null,
+                        null, null, null, null);
+
+                cursor.moveToPosition(i);
+
+                //insertRecipe(db,"Chicago-Style Deep Dish Pizza with Italian Sausage",String.valueOf(R.string.Deep_Dish),"MEDIUM","02:55:00",R.drawable.chicago_style_deep_dish);
+                //int recipeId = cursor.getInt(0);
+                //System.out.println(recipeId);
+                String name = cursor.getString(1);
+                System.out.println(name);
+                String steps = cursor.getString(2);
+                System.out.println(steps);
+                String level = cursor.getString(3);
+                System.out.println(level);
+                String time = cursor.getString(4);
+                System.out.println(time);
+                int imageId = cursor.getInt(5);
+                System.out.println(imageId);
+                TextView recipe_textview;
+                Button recipe_button;
+
+
+                switch (i + 1) {
+                    case 1:
+                        recipe_textview = (TextView) findViewById(R.id.recipe_1_textview);
+                        recipe_button = (Button) findViewById(R.id.recipe_1_button);
+                        break;
+                    case 2:
+                        recipe_textview = (TextView) findViewById(R.id.recipe_2_textview);
+                        recipe_button = (Button) findViewById(R.id.recipe_2_button);
+                        break;
+                    case 3:
+                        recipe_textview = (TextView) findViewById(R.id.recipe_3_textview);
+                        recipe_button = (Button) findViewById(R.id.recipe_3_button);
+                        break;
+                    case 4:
+                        recipe_textview = (TextView) findViewById(R.id.recipe_4_textview);
+                        recipe_button = (Button) findViewById(R.id.recipe_4_button);
+                        break;
+                    case 5:
+                        recipe_textview = (TextView) findViewById(R.id.recipe_5_textview);
+                        recipe_button = (Button) findViewById(R.id.recipe_5_button);
+                        break;
+                    case 6:
+                        recipe_textview = (TextView) findViewById(R.id.recipe_6_textview);
+                        recipe_button = (Button) findViewById(R.id.recipe_6_button);
+                        break;
+                    default:
+                        recipe_textview = (TextView) findViewById(R.id.recipe_1_textview);
+                        recipe_button = (Button) findViewById(R.id.recipe_1_button);
+                        break;
+
+                }
+                recipe_textview.setText(name);
+
+                Drawable img = recipe_button.getContext().getResources().getDrawable(imageId);
+                recipe_button.setCompoundDrawablesWithIntrinsicBounds(img, null, null, null);
+
+                cursor = db.query("RECIPEITEMS",
+                        new String[]{"*"},
+                        "RECIPE_ID = ?",
+                        new String[]{Integer.toString(i + 1)}, null, null, null);
+                cursor.moveToFirst();
+
+                String recipe_text = "Ingredients:\n";
+                recipe_button.setTextSize(10);
+
+
+                for (int j = 0; j <= 6; j++) {
+                    if (j > 0) {
+                        cursor.moveToNext();
+                    }
+                    recipe_text += "• ";
+                    String tempName = cursor.getString(1);
+                    Double tempWeight = cursor.getDouble(2);
+                    String tempType = cursor.getString(3);
+                    if (!tempType.equals("ounces")) {
+                        if (tempType.equals("teaspoon"))
+                            recipe_text += convertOuncesToTeaspoon(tempWeight) + " " + tempType + " of " + tempName + "\n";
+                        else
+                            recipe_text += convertOuncesToTablespoon(tempWeight) + " " + tempType + " of " + tempName + "\n";
+                    }//end if
+                    else {
+                        recipe_text += tempWeight + " " + tempType + " of " + tempName + "\n";
+                    }//end else
+                }//end for loop
+                recipe_text += "Time : " + time + "\t\t" + "Level: " + level;
+                recipe_button.setText(recipe_text);
+                cursor.close();
+                db.close(); //close the database
+            } catch (SQLiteException e) {
                 Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT); //Display a pop-up message if a SQLiteException is thrown
                 toast.show();
+            }
+
         }
+
+
+
+        /*plantryDatabaseHelper = new PlantryDatabaseHelper(this);
+            db = plantryDatabaseHelper.getReadableDatabase();
+            cursor = db.query("RECIPE",
+                    new String[]{"*"},
+                    null,
+                    null, null, null, null);
+        */
+
+
     }
+
 
     public void goToPantry(View view) {
         Intent intent = new Intent(this, PantryActivity.class);
