e30bc087be661ec03bdd82e0a6005d7213f286c2
lucianogiannini <54635097+lucianogiannini@users.noreply.github.com>
2021-04-25 15:06:32 -0400

    updating activity_shopping_list.xml

--- Plantry/app/src/main/java/com/hfad/plantry/ShoppingListActivity.java
+++ Plantry/app/src/main/java/com/hfad/plantry/ShoppingListActivity.java
@@ -2,7 +2,9 @@ package com.hfad.plantry;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ShoppingListActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class ShoppingListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping_list);
+    }
+
+    public void goBackToHome(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent); //start new activity
     }
 }
